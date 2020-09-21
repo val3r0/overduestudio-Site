b2730acb249ea5a6790db68c9e962070394a6f80
Daniel Garcia Valero <daniel@Daniels-iMac.local>
2020-09-22 09:04:23 +1000

    SEO

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 import Body from "../components/body";
+import SEO from "../components/seo";
 
 
 function App() {
-    return (
+    return (  
     <div>
+        <SEO 
+        title="Overdue Studio"
+        description="Overdue studio is a visual design studio creating thoughtful design digitally and physically."/>
         <Body></Body>
     </div>
     ) 
