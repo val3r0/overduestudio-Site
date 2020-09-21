77d5cc26e10cc29c4783e58a8ca54562c73d3e0c
Daniel Garcia Valero <daniel@Daniels-iMac.local>
2020-09-22 07:43:51 +1000

    Analytics added

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,14 +5,30 @@
  */
 
 module.exports = {
+  /* SEO */
+  siteMetadata: {
+    title: "Overdue Studio",
+    description: "Overdue studio is a visual design studio creating thoughtful design digitally and physically",
+    url: "https://www.overduestudio.co", // No trailing slash allowed!
+    image: "/social_media_preview.jpg", // Path to your image you placed in the 'static' folder
+    author: "Overdue Studio"
+  },
   /* Your site config here */
   plugins: [
     `gatsby-plugin-smoothscroll`,
     `gatsby-plugin-react-helmet`,
     {
-      resolve: `gatsby-plugin-styled-components`,
-      options: {
+    resolve: `gatsby-plugin-styled-components`,
+    options: {
         // Add any options here
+      },
+    },  
+    {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+        // replace "UA-XXXXXXXXX-X" with your own Tracking ID
+        trackingId: "UA-57221889-2",
+        head: true,
       },
     },
   ],
